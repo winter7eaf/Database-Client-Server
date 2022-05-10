3166cf0023ed07ec4b2756352d4241ac226c492a
Catrin <95254982+winter7eaf@users.noreply.github.com>
2022-05-10 12:16:49 +0100

    Add files via upload

--- src/Credentials.java
+++ src/Credentials.java
@@ -2,10 +2,10 @@
 
 public class Credentials {
     //JDBC connection
-    public static final String USERNAME = "catrin_fsad";
-    public static final String PASSWORD = "password";
-    public static final String URL = "jdbc:postgresql://localhost:5432/Music";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String URL = "";
     //Client-server connection
     public static final String HOST = "127.0.0.1"; //localhost
-    public static final int PORT = 8080;
+    public static final int PORT = 80;
 }
